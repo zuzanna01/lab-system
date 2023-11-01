b0135da89aadd01a28cc4dde6202922e378ffd4b
Zuzanna <01158303@pw.edu.pl>
2023-11-01 15:53:37 +0100

    first commit

---
+++ src/main/java/pl/edu/pw/zpoplaws/labsystem/LabSystemApplication.java
@@ -0,0 +1,13 @@
+package pl.edu.pw.zpoplaws.labsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LabSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LabSystemApplication.class, args);
+    }
+
+}
